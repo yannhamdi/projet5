b2c2898cba0a0a72dccb5c0af1bf4844b2cb9be9
YANN HAMDI <hamdiyann@gmail.com>
2018-02-10 17:41:27 +0100

    rajout module base de données

---
+++ database.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+"module that manages the database"
+
+import records
+
+class Data_base:
+    def __init__(self):
+        "connection to our database openfood"
+        self.db = records.Database('mysql+pymysql://root:yh250980@localhost/openfood?charset=utf8mb4')
+        self.category_display()
+
+
+
+    def category_display(self):
+        "function that shows the different category"
+        self.rows = self.db.query("SELECT * FROM categorie")
+        
+
+
+
+if __name__ == '__main__':
+    main()
