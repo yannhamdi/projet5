910366de8f98b21012b4480fbf656ab125780de9
YANN HAMDI <hamdiyann@gmail.com>
2018-02-10 17:40:52 +0100

    travail sur interaction

--- interaction.py
+++ interaction.py
@@ -1,53 +1,56 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 # -*- coding: Utf-8 -*
-
 
 import records
 
+from database import *
 
-class User_choice:
+
+class User_choice(Data_base):
     "CLass that interacts with the user"
     def __init__(self):
-        self.db = records.Database('mysql+pymysql://root:yh250980@localhost/openfood?charset=utf8mb4')
-        #we ask the user to choose what he wants to do
-        self.choice = input("1-Quels aliments souhaitez-vous remplacer\n2- Retrouver vos aliments substitués")
-        self.choice = int(self.choice)
-        # while the user choice is not 1 or 2 we reiterate the question
-        if self.choice == 1: 
-            rows = self.db.query("SELECT * FROM categorie")
-            for r in rows:
-                print(r.id, r.cat)
-            self.choice_category = input("Sélectionnez la catégorie")
-            self.choice_category = int(self.choice_category)
-            self.display_food_name()
-        else:
-            while self.choice != 2:
-                self.choice = input("1-Quels aliments souhaitez-vous remplacer\n2- Retrouver vos aliments substitués")
+        " constructor that create the list to double check the user choice"
+        self.id_category = []
+        self.id_included =[]
+        self.id_first_choice = [1,2]
+        self.row_category = self.db.query("SELECT id FROM categorie")
+        for categ in self.row_category:
+            self.id_category.append(categ.id)
+        self.row_name = self.db.query("SELECT id FROM food")
+        for nam in self.row_name:
+            self.id_included.append(nam.id)
     
-
-
-
-    
-    def display_food_name(self):
-        "function that displays the product by category"
-        id_included = []
-        rows_food = self.db.query("SELECT id, food_name FROM food WHERE category_id ={choice}".format(choice = self.choice_category))
-        for product in rows_food:
-            print(product.id, product.food_name)
-            id_included.append(product.id)
-        choice_food = input ("Sélectionnez numéro de l'aliment à substituer")
-        choice_food = int(choice_food)
-        if choice_food in id_included:
-            print("ok")
+    def checking_choice(self, choice, liste):
+        "function that checks the user choice"
+        if choice in liste:
+            return 1
         else:
-            while choice_food not in id_included:
-                choice_food = input("Sélectionnez numéro de l'aliment à substituer") 
-                choice_food = int(choice_food)
-                if choice_food in id_included:
+            return 0
+
+    def first_choice(self):
+        "displays the first introdution to the program"
+        while True:
+            try:
+                self.choice = int(input("alors"))
+                if self.checking_choice(self.choice, self.id_first_choice) == 1:
                     break
+            except:
+                True
+        self.second_choice(self.choice)
+    def second_choice(self, choice):
+        "second degree display choice"
+        if choice == 1:
+            for r in Data_base.rows:
+                print(r.id, r.cat)
+
+            	
+
+   
+
 
 def main():
     user_choice = User_choice()
+    user_choice.first_choice()
 main()
 
 
