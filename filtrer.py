e43e608864706a90cc31699896233a973cc768f1
YANN HAMDI <hamdiyann@gmail.com>
2018-02-13 13:02:49 +0100

    mise au norme pylint le fichier filtrer.py

--- filtrer.py
+++ filtrer.py
@@ -5,10 +5,8 @@
 
 
 import pandas as pd
-  
 
 import records
-
 
 from database import *
 
@@ -19,25 +17,29 @@ class Data_Creating(Data_base):
         "we initialize our file which is going to be usefull for our database"
         Data_base.__init__(self)
         # I create a list of the columns needed for my database
-        self.my_columns = ["code", "url", "product_name", "stores", "nutrition_grade_fr", "main_category_fr"]
+        self.my_columns = ["code", "url", "product_name", "stores",
+                           "nutrition_grade_fr", "main_category_fr"]
         # We open our csv file
-        df = pd.read_csv("openfood.csv", sep = "\t", low_memory = False)
-        self.my_file = (df.loc[(df["countries"] == "France") & ((df["main_category_fr"] == "Jus de fruits")
-                 | (df["main_category_fr"] =="Charcuteries") | (df["main_category_fr"] == "Snacks sucrés")
-                                     | (df["main_category_fr"] == "Soupes")
-                                     | (df["main_category_fr"] == "Fromages") |
-                                        (df["main_category_fr"] == "Plats préparés"))
-                                     & ((df["nutrition_grade_fr"] == "a")| (df["nutrition_grade_fr"] == "b")
-                                     | (df["nutrition_grade_fr"] == "c")| (df["nutrition_grade_fr"] == "d")
-                                     | (df["nutrition_grade_fr"] == "a")), self.my_columns]).dropna()
-       
-        self.my_file.to_csv("myFile.csv", sep = "\t")
+        df = pd.read_csv("openfood.csv", sep="\t", low_memory=False)
+        self.my_file = (df.loc[(df["countries"] == "France") &
+                               (df["main_category_fr"] == "Jus de fruits")
+                               | (df["main_category_fr"] == "Charcuteries") |
+                               (df["main_category_fr"] == "Snacks sucrés")
+                               | (df["main_category_fr"] == "Soupes")
+                               | (df["main_category_fr"] == "Fromages") |
+                               (df["main_category_fr"] == "Plats préparés")
+                               & ((df["nutrition_grade_fr"] == "a")
+                                  | (df["nutrition_grade_fr"] == "b")
+                                  | (df["nutrition_grade_fr"] == "c")
+                                  | (df["nutrition_grade_fr"] == "d")
+                                  | (df["nutrition_grade_fr"] == "a")), self.my_columns]).dropna()
+        self.my_file.to_csv("myFile.csv", sep="\t")
 
     def create_data_base(self):
         "we filling up our database"
         id_category = 0
         self.db.query("DELETE FROM food")
-        file_data = pd.read_csv("myFile.csv", sep = "\t", low_memory = False)
+        file_data = pd.read_csv("myFile.csv", sep="\t", low_memory=False)
         for index, row in file_data.iterrows():
             if row["main_category_fr"] == "Jus de fruits":
                 id_category = 1
@@ -51,13 +53,17 @@ class Data_Creating(Data_base):
                 id_category = 5
             elif row["main_category_fr"] == "Plats préparés":
                 id_category = 6
-            self.db.query("""INSERT INTO food(id_openfood, food_link, food_name, store, nutrition_grade , category_id) VALUES(:id_openfood, :food_link, :food_name, :store, :nutrition_grade, :category_id)""",
-            id_openfood = row["code"], food_link = row["url"], food_name = row["product_name"], store = row["stores"], nutrition_grade = row["nutrition_grade_fr"], category_id = id_category)    
-    
+            self.db.query("""INSERT INTO
+                    food(id_openfood, food_link, food_name, store, nutrition_grade , category_id) 
+                          VALUES(:id_openfood, :food_link, :food_name, :store, :nutrition_grade, 
+                          :category_id)""",
+                          id_openfood=row["code"], food_link=row["url"],
+                          food_name=row["product_name"], store=row["stores"],
+                          nutrition_grade=row["nutrition_grade_fr"], category_id=id_category)
+
 
 def main():
+    "main function that insrances the object of the class Data_Creating"
     data_create = Data_Creating()
     data_create.create_data_base()
-    
-    
 main()
