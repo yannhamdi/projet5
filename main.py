11c3bd2dcad229b62a344eae5d7daeab20f69ecc
YANN HAMDI <hamdiyann@gmail.com>
2018-02-15 13:14:28 +0100

    ajout doc string sur main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: Utf-8 -*
+
+"function main which will launche our application"
+
+
 from interaction import *
 
 
-
 def main():
+    "function that instances the User_choice class"
     user_choice = User_choice()
     user_choice.first_choice()
 main()
