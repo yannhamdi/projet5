6858ad7ea2a4b58fff799b9f62f600c9cc8e8a7b
YANN HAMDI <hamdiyann@gmail.com>
2018-02-19 12:45:10 +0100

    ajout sur fichier livrable

--- food_queries.py
+++ food_queries.py
@@ -9,7 +9,6 @@ from database import *
 from interaction import *
 
 
-
 class Food_queries():
     "class that manages queries of products"
     def __init__(self):
@@ -24,15 +23,9 @@ class Food_queries():
             if self.foods.category_id == second_choice:
                 print(self.foods.id_openfood, self.foods.food_name, self.foods.nutrition_grade)
 
-
-
-    
-
-
     def saving_in_database(self, choice_to_substitute, sub_choice):
         "function that save the user search in database"
         self.saving = self.data_base.db.query("INSERT INTO User_search(id_to_substitue, id_substitued) VALUES(:id_to_substitue, :id_substitued)", id_to_substitue=choice_to_substitute, id_substitued=sub_choice)
-
 
 
 if __name__ == '__main__':
